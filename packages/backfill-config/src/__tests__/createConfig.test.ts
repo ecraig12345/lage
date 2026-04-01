7a24cf61b38c64829d51c6128d7d119fd5a26460
Elizabeth Craig <elcraig@microsoft.com>
2026-04-01 14:25:20 -0700

    Add more caching tests and remove no-op stats in LocalCacheProvider (#1087)

--- packages/backfill-config/src/__tests__/createConfig.test.ts
+++ packages/backfill-config/src/__tests__/createConfig.test.ts
@@ -1,7 +1,7 @@
 import {
   afterAll,
   afterEach,
-  beforeAll,
+  beforeEach,
   describe,
   expect,
   it,
@@ -69,10 +69,11 @@ describe("getSearchPaths()", () => {
 
 describe("createConfig()", () => {
   const originalEnv = process.env;
-  const logger = makeLogger("info");
+  const logger = makeLogger("mute");
   let packageRoot = "";
 
-  beforeAll(() => {
+  beforeEach(() => {
+    // Note: jest-setup.js clears all BACKFILL_ vars that might have come from CI
     process.env = { ...originalEnv };
   });
 
