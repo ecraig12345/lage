b5fe7bc91e19834f5869d38f048eddb2f0195ce0
Elizabeth Craig <elcraig@microsoft.com>
2026-06-24 19:02:05 -0700

    wip lock reading fix from 2026-03

--- packages/workspace-tools/src/lockfile/parseBerryLock.ts
+++ packages/workspace-tools/src/lockfile/parseBerryLock.ts
@@ -6,21 +6,19 @@ import type { LockDependency, ParsedLock, BerryLockFile } from "./types.js";
 export function parseBerryLock(yaml: BerryLockFile): ParsedLock {
   const results: { [key: string]: LockDependency } = {};
 
-  if (yaml) {
-    for (const [keySpec, descriptor] of Object.entries(yaml)) {
-      if (keySpec === "__metadata") {
-        continue;
-      }
+  for (const [keySpec, descriptor] of Object.entries(yaml)) {
+    if (keySpec === "__metadata") {
+      continue;
+    }
 
-      const keys = keySpec.split(", ");
+    const keys = keySpec.split(", ");
 
-      for (const key of keys) {
-        const normalizedKey = normalizeKey(key);
-        results[normalizedKey] = {
-          version: descriptor.version,
-          dependencies: descriptor.dependencies ?? {},
-        };
-      }
+    for (const key of keys) {
+      const normalizedKey = key.replace("@npm:", "@");
+      results[normalizedKey] = {
+        version: descriptor.version,
+        dependencies: descriptor.dependencies ?? {},
+      };
     }
   }
 
@@ -28,13 +26,4 @@ export function parseBerryLock(yaml: BerryLockFile): ParsedLock {
     object: results,
     type: "success",
   };
-}
-
-// normalizes the version range as a key lookup
-function normalizeKey(key: string): string {
-  if (key.includes("npm:")) {
-    return key.replace(/npm:/, "");
-  }
-
-  return key;
 }
--- packages/workspace-tools/src/lockfile/parsePnpmLock.ts
+++ packages/workspace-tools/src/lockfile/parsePnpmLock.ts
@@ -6,18 +6,44 @@ export function parsePnpmLock(yaml: PnpmLockFile): ParsedLock {
     [key in string]: LockDependency;
   } = {};
 
-  if (yaml && yaml.packages) {
-    for (const [pkgSpec, snapshot] of Object.entries(yaml.packages)) {
-      // TODO: handle file:foo.tgz syntax (rush uses this for internal package links)
-      const specParts = pkgSpec.split(/\//);
-      const name = specParts.length > 3 ? `${specParts[1]}/${specParts[2]}` : specParts[1];
-      const version = specParts.length > 3 ? specParts[3] : specParts[2];
+  if (!yaml.packages) {
+    return { object, type: "success" };
+  }
 
-      object[nameAtVersion(name, version)] = {
-        version,
-        dependencies: snapshot.dependencies,
-      };
+  // All versions: https://github.com/pnpm/spec/blob/master/lockfile/README.md
+  // Unfortunately the dependency path spec for each version is not as clear as it could be...
+  const lockfileVersion = Number(yaml.lockfileVersion.match(/^\d+/)?.[0]);
+  if (!lockfileVersion || lockfileVersion > 9 || lockfileVersion < 5) {
+    // A new lockfileVersion might have changed the key format again
+    throw new Error(`Unsupported pnpm lockfile version: ${yaml.lockfileVersion}`);
+  }
+
+  for (let [pkgSpec, snapshot] of Object.entries(yaml.packages)) {
+    let name: string;
+    let version: string;
+    if (lockfileVersion === 5) {
+      // /@types/node/20.19.33 or add scoped parts after another /
+      // TODO: handle file:foo.tgz syntax (rush uses this for internal package links)
+      const specParts = pkgSpec.split("/");
+      name = specParts.length > 3 ? `${specParts[1]}/${specParts[2]}` : specParts[1];
+      version = specParts.length > 3 ? specParts[3] : specParts[2];
     }
+
+    // pnpm 8 format (lockfileVersion 6):     /@types/node@20.19.33 or /@rushstack/k@1.0.0(@rushstack/m@1.0.0)(@rushstack/n@2.0.0)
+    // pnpm 9+ format (lockfileVersion 9):    @types/node@20.19.33
+    if (lockfileVersion < 6) {
+      // pnpm < 8 format (lockfileVersion < 6):
+    } else if (lockfileVersion === 6) {
+    } else {
+    }
+    if (lockfileVersion < 9 && pkgSpec.startsWith("/")) {
+      pkgSpec = pkgSpec.slice(1);
+    }
+
+    object[nameAtVersion(name, version)] = {
+      version,
+      dependencies: snapshot.dependencies,
+    };
   }
 
   return {
